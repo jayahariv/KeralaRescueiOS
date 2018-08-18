9ee09c78940fe017d6b4ff7e44bbc984185a4792
Akhil <akhilms225@gmail.com>
2018-08-18 01:35:55 -0700

    Adding detail view controller...

---
+++ RescueApp/RescueApp/DetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.swift
+//  RescueApp
+//
+//  Created by Akhil MS on 18/08/18.
+//  Copyright © 2018 Jayahari Vavachan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+}
