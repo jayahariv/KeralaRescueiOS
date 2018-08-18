f1d8a65ed8b6a23ec44b186386f23f808a66fce8
Ajith Antony <ajith.antony@rapidvaluesolutions.com>
2018-08-17 20:00:27 -0700

    draw route

--- RescueApp/RescueApp/ResourceNeedsMapViewController.swift
+++ RescueApp/RescueApp/ResourceNeedsMapViewController.swift
@@ -12,7 +12,6 @@ import MapKit
 class ResourceNeedsMapViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
-    
     private var requests = [RequestModel]()
     
     struct C {
@@ -22,8 +21,14 @@ class ResourceNeedsMapViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        initialise()
         getResources()
+    }
+
+    func initialise() {
+        //set delegate for mapview
+        self.mapView.delegate = self
     }
     
     @IBAction func onTouchUpList(_ sender: UIButton) {
@@ -37,7 +42,6 @@ class ResourceNeedsMapViewController: UIViewController {
     }
 
 }
-
 
 extension ResourceNeedsMapViewController {
     func getResources() {
@@ -55,5 +59,41 @@ extension ResourceNeedsMapViewController {
         let allAnnotations = mapView.annotations
         mapView.removeAnnotations(allAnnotations)
         mapView.addAnnotations(requests)
+    }
+
+    func showDirection(sourceLocation: CLLocationCoordinate2D, destinationLocation: CLLocationCoordinate2D) {
+        let sourcePlaceMark = MKPlacemark(coordinate: sourceLocation)
+        let destinationPlaceMark = MKPlacemark(coordinate: destinationLocation)
+
+        let directionRequest = MKDirectionsRequest()
+        directionRequest.source = MKMapItem(placemark: sourcePlaceMark)
+        directionRequest.destination = MKMapItem(placemark: destinationPlaceMark)
+        directionRequest.transportType = .automobile
+
+        let directions = MKDirections(request: directionRequest)
+        directions.calculate { (response, error) in
+            guard let directionResonse = response else {
+                if let error = error {
+                    print("we have error getting directions==\(error.localizedDescription)")
+                }
+                return
+            }
+
+            let route = directionResonse.routes[0]
+            self.mapView.add(route.polyline, level: .aboveRoads)
+
+            let rect = route.polyline.boundingMapRect
+            self.mapView.setRegion(MKCoordinateRegionForMapRect(rect), animated: true)
+        }
+    }
+}
+
+extension ResourceNeedsMapViewController: MKMapViewDelegate {
+    //MARK:- MapKit delegates
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = UIColor.blue
+        renderer.lineWidth = 4.0
+        return renderer
     }
 }
