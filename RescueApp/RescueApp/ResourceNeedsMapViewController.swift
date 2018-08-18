2dec0e08088148246053c445817e9fed5c0b1713
Jayahari Vavachan <10448770+jayahariv@users.noreply.github.com>
2018-08-17 20:15:56 -0700

    feat: Direct Map
    * show direct mapview as home view
    * current location started using

--- RescueApp/RescueApp/ResourceNeedsMapViewController.swift
+++ RescueApp/RescueApp/ResourceNeedsMapViewController.swift
@@ -13,17 +13,22 @@ class ResourceNeedsMapViewController: UIViewController {
     
     @IBOutlet weak var mapView: MKMapView!
     
+    
     private var requests = [RequestModel]()
+    private let locationManager = CLLocationManager()
     
     struct C {
         static let animationIdentifier = "ResourceListViewControllerFlip"
         static let ResourceListViewController = "ResourceNeedsListViewController"
+        static let mapAnnotationIdentifier = "MapAnnotationIdentifier"
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         getResources()
+        
+        getCurrentLocation()
     }
     
     @IBAction func onTouchUpList(_ sender: UIButton) {
@@ -55,5 +60,60 @@ extension ResourceNeedsMapViewController {
         let allAnnotations = mapView.annotations
         mapView.removeAnnotations(allAnnotations)
         mapView.addAnnotations(requests)
+    }
+    
+    /**
+     gets current location.
+     */
+    func getCurrentLocation() {
+        locationManager.requestAlwaysAuthorization()
+        locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+            locationManager.startUpdatingLocation()
+        }
+    }
+}
+
+// MARK: AddToiletViewController -> CLLocationManagerDelegate
+
+extension ResourceNeedsMapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locationManager.stopUpdatingLocation()
+        if let location = manager.location {
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = location.coordinate
+            mapView.addAnnotation(annotation)
+            let region = MKCoordinateRegion(center: location.coordinate,
+                                            span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02))
+            mapView.setRegion(region, animated: true)
+        }
+        
+    }
+}
+
+// MARK: MapViewController -> MKMapViewDelegate
+
+extension ResourceNeedsMapViewController: MKMapViewDelegate {
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        guard let annotation = annotation as? RequestModel else { return nil }
+        
+        var view: MKMarkerAnnotationView
+        
+        if let dequeuedView = mapView.dequeueReusableAnnotationView(withIdentifier: C.mapAnnotationIdentifier)
+            as? MKMarkerAnnotationView {
+            dequeuedView.annotation = annotation
+            view = dequeuedView
+        } else {
+            
+            view = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: C.mapAnnotationIdentifier)
+            view.canShowCallout = true
+            view.calloutOffset = CGPoint(x: -5, y: 5)
+            view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        }
+        return view
     }
 }
