4754c45f949e3c14f5d39b95ae56491bb6f401ac
Jayahari Vavachan <10448770+jayahariv@users.noreply.github.com>
2018-08-19 22:03:24 -0700

    feat: Filter Add
    * add class for filtering
    * planning to show it as a model

--- RescueApp/RescueApp/ViewControllers/RequestFilterViewController.swift
+++ RescueApp/RescueApp/ViewControllers/RequestFilterViewController.swift
@@ -4,34 +4,50 @@ RequestFilterViewController.swift
 Created on: 19/8/18
 
 Abstract:
-TODO: Purpose of file
+ Requests filters are controlled inside this class.
 
 */
 
 import UIKit
 
+enum FilterTypes {
+    case location
+}
+
+protocol RequestFilterProtocol {
+    func didFiltersSelected(filters: [FilterTypes])
+}
+
 class RequestFilterViewController: UIViewController {
+    
+    @IBOutlet private weak var navigationBar: UINavigationBar!
+    
+    // MARK: View Lifecycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        configureUI()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: Button Actions
+    @IBAction func onCancel(_ sender: Any) {
+        
     }
     
+    @IBAction func onApply(_ sender: Any) {
+        
+    }    
+}
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+// MARK: Helper methods
+
+private extension RequestFilterViewController {
+    /**
+     here all the UI is configured once when view is loaded
+     */
+    func configureUI() {
+        navigationBar.titleTextAttributes = [NSAttributedStringKey.foregroundColor: UIColor.white]
     }
-    */
-
 }
