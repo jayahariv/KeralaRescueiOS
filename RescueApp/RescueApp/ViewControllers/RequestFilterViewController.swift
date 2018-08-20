293c6f8d29c855b2a879755c1d063c18aee9d3ca
Jayahari Vavachan <10448770+jayahariv@users.noreply.github.com>
2018-08-19 21:54:58 -0700

    refactor: folder structuring
    * folder restructured

---
+++ RescueApp/RescueApp/ViewControllers/RequestFilterViewController.swift
@@ -0,0 +1,37 @@
+//
+/*
+RequestFilterViewController.swift
+Created on: 19/8/18
+
+Abstract:
+TODO: Purpose of file
+
+*/
+
+import UIKit
+
+class RequestFilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
